f73f07487d589e965ff554665ddd9f9607b34e7f
diBarraza <dbarraza.2858@gmail.com>
2023-12-09 10:15:32 -0300

    back->feat(configure connection): The connection with the front-end is configured.

--- guardian_backend/features/views.py
+++ guardian_backend/features/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def mi_vista_json(request):
-    data = {'mensaje': 'Hola desde Django JSON!'}
+    data = {'mensaje': 'Hola amor mio como estas?!'}
     return JsonResponse(data)
 
 # Create your views here.
