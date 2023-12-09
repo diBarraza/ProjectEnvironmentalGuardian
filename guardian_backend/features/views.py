bb944a846c4fcc0b4a52dfd720b0e1dda0775a22
diBarraza <dbarraza.2858@gmail.com>
2023-12-09 19:20:36 -0300

    feat(update message): edit message from back-end.

--- guardian_backend/features/views.py
+++ guardian_backend/features/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def mi_vista_json(request):
-    data = {'mensaje': 'Hola amor mio como estas?!'}
+    data = {'mensaje': 'First message from Django!'}
     return JsonResponse(data)
 
 # Create your views here.
